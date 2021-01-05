a078d4ded5014d7480ce66696976a56b768c0963
Apple <apple@MacbookPro.local>
2021-01-05 01:29:58 +0000

    services phase 1

--- js/app.js
+++ js/app.js
@@ -8,12 +8,28 @@ const para02 = document.querySelector('.para-02');
 const para03 = document.querySelector('.para-03');
 const excerpt01 = document.querySelector('.excerpt-01');
 const excerpt02 = document.querySelector('.excerpt-02');
-const excerpt03 = document.querySelector('.except-03');
+const excerpt03 = document.querySelector('.excerpt-03');
+const description = document.querySelectorAll('.box .description');
 
+
+function removeFlex(){
+    console.log('IN')
+    description.forEach((item)=>{
+        item.style.display === 'block' ? item.style.display = 'flex' :
+        item.style.display = 'block'
+        // item.style.display = 'display: block'
+        console.log(item.style.display)
+        });
+}
+function topLink(){
+        location.href = '#what-we-do';
+}
 
 gridContainer.addEventListener(('click'), (e)=>{
     if (e.target.className === 'more-btn-01'){
-        moreBtn01.innerHTML === 'More' ? moreBtn01.innerHTML = 'Less' :
+    //    removeFlex();
+        moreBtn01.innerHTML === 'More' ? 
+        (moreBtn01.innerHTML = 'Less', topLink()) :
             moreBtn01.innerHTML = 'More';
     para01.classList.toggle('hide');
     excerpt01.innerHTML.length > 1 ? excerpt01.innerHTML = "" :
@@ -21,6 +37,7 @@ gridContainer.addEventListener(('click'), (e)=>{
 
     }
     if (e.target.className === 'more-btn-02'){
+        // removeFlex();
         moreBtn02.innerHTML === 'More' ? moreBtn02.innerHTML = 'Less' :
             moreBtn02.innerHTML = 'More';
     para02.classList.toggle('hide');
@@ -28,6 +45,7 @@ gridContainer.addEventListener(('click'), (e)=>{
         excerpt02.innerHTML = ".........";
     }
     if (e.target.className === 'more-btn-03'){
+        // removeFlex();
         moreBtn03.innerHTML === 'More' ? moreBtn03.innerHTML = 'Less' :
             moreBtn03.innerHTML = 'More';
     para03.classList.toggle('hide');
